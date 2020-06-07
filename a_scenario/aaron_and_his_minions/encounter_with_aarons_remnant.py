f2d115811c252f2b1a8516d88324a08e195e94ed
Dynogoat <elicrow2008@gmail.com>
2020-06-06 20:56:59 -0500

    Aarons Remnant attack

--- a_scenario/aaron_and_his_minions/encounter_with_aarons_remnant.py
+++ a_scenario/aaron_and_his_minions/encounter_with_aarons_remnant.py
@@ -1,11 +1,27 @@
 import random
-player_health = 40
+player_health = 100
 print("""A ghostly top half of a body with and evil grin
 on his face, and a thin goatee floats over to you your
 body shall be my new vessel in this world. He laughs at
 you as a blue icy flame appears in his hand.""")
-aarons_remnant_health = random.randint(15,47)
-aarons_remnant_attack = random.randint(1,4)
+aarons_remnant_health = random.randint(15, 47)
+aarons_remnant_attack = random.randint(1, 4)
 if aarons_remnant_attack <= 3:
     print("""Aarons Remnant conjures and icy blue flame
-and tosses it at you.""")
+and tosses it at you, you feel a cold burning feeling
+all over your body.""")
+    aarons_remnant_damage = random.randint(20, 25)
+    print("you take " + str(aarons_remnant_damage) + " damage.")
+    player_health -= aarons_remnant_damage
+elif aarons_remnant_attack == 4:
+    print("""Aarons Remnant flies over to you and reaches into
+your body, you feel a cold hand pulling at your heart. """)
+    con_saving_throw = random.randint(1, 20)
+    if con_saving_throw >= 11:
+        print("you are mostly unharmed and feel nothing else.")
+    elif con_saving_throw < 11 and con_saving_throw >=2:
+        aarons_remnant_damage = random.randint(30-40)
+        print("""you feel a a cold pulling sensation in your
+heart as it moves in ways it shouldn't, and you take """
++ str(aarons_remnant_damage) + """ damage""")
+        player_health -= aarons_remnant_damage
