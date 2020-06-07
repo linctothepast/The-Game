27f61e24a83feef8fa2c97b0415f1761ad2cc92a
Dynogoat <elicrow2008@gmail.com>
2020-06-06 19:41:51 -0500

    Just started programming aarons remnant encounter.

---
+++ a_scenario/aaron_and_his_minions/encounter_with_aarons_remnant.py
@@ -0,0 +1,6 @@
+import random
+print("""A ghostly top half of a body with and evil grin
+on his face, and a thin goatee floats over to you your
+body shall be my new vessel in this world. He laughs at
+you as a blue icy flame appears in his hand""")
+aarons_remnant_health = random.randint(15,47)
