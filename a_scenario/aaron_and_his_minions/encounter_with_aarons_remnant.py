4f74cbfe0e773c4a5651098bf76208b1a9ac1d09
Dynogoat <elicrow2008@gmail.com>
2020-06-06 20:09:45 -0500

    Aarons Remnant first attack update and health update

--- a_scenario/aaron_and_his_minions/encounter_with_aarons_remnant.py
+++ a_scenario/aaron_and_his_minions/encounter_with_aarons_remnant.py
@@ -1,6 +1,11 @@
 import random
+player_health = 40
 print("""A ghostly top half of a body with and evil grin
 on his face, and a thin goatee floats over to you your
 body shall be my new vessel in this world. He laughs at
-you as a blue icy flame appears in his hand""")
+you as a blue icy flame appears in his hand.""")
 aarons_remnant_health = random.randint(15,47)
+aarons_remnant_attack = random.randint(1,4)
+if aarons_remnant_attack <= 3:
+    print("""Aarons Remnant conjures and icy blue flame
+and tosses it at you.""")
